6e43a5d958c28820c3c2427d3fac3a119ea66a5d
AuyeskhanM <auyeskhanm@gmail.com>
2019-04-28 19:51:11 +0600

    cx

---
+++ Music/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Music
+{
+    public class Entity
+    {
+
+        public Guid Id { get; set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ Music/Song.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Music
+{
+
+    public class Song : Entity
+    {
+        public string Name { get; set; }
+        public long DurationSeconds { get; set; }
+        public int Rating { get; set; }
+        public Band Band { get; set; }
+        public ICollection<string> Words { get; set; } = new List<string>();
+    
+        public void Print()
+        {
+            Console.WriteLine($"Title: {Name}");
+            long mins, seconds;
+            mins = DurationSeconds / 60;
+            seconds = DurationSeconds % 60;
+            Console.WriteLine($"Duration: {mins}:{seconds}");
+            Console.WriteLine($"Rating: {Rating}");
+            Console.WriteLine($"Executor: {Band.Name}");
+            Console.WriteLine($"Words: ");
+            foreach (var word in Words)
+            {
+                Console.Write(word + " ");
+            }
+        }
+    }
+}
